a5677c4e2f583148d695866a1b4944e4e9ce4cb8
Evan Summers <evan.summers@gmail.com>
2016-05-02 05:57:45 +0200

    readme

--- bin/_util.sh
+++ bin/_util.sh
@@ -4,11 +4,212 @@ set -u -e
 
 concerto=$0
 
+# supported clients
+
 client=${client:=redishub}
 client_authorized_certs=${client}_authorized_certs
 
-. bin/_${client}.sh
-. bin/_util.sh 
+. bin/_${client}.sh 
+
+# bash echo styles and colors
+
+sBold="\e[1m"
+sItalic="\e[3m"
+sUnderline="\e[4m"
+sReset="\e[0m"
+
+cNormal="\e[39m"
+cRed="\e[91m"
+cGreen="\e[32m"
+cYellow="\e[33m"
+cLightGray="\e[37m"
+cDarkGray="\e[90m"
+cLightBlue="\e[94m"
+
+# environment
+
+debug() {
+  local name="$1"
+  shift
+  echo -e "${cDarkGray}$name ${cDarkGray}${sBold}${*}${sReset}"
+}
+
+# prepare $archive dir for today
+
+archive=$priv/tmp/archive/`date +%Y%m%d`
+mkdir -p $archive
+#mv -f $priv/tmp/* $archive/. 2> /dev/null || true
+
+# prepare $id defaulted to HOST-USER-DATE-TIME
+
+user=${user:=none}
+if [ -f $priv/github.user ]
+then
+   user=`cat $priv/github.user`
+   [ -t 1 ] && echo -e "${cDarkGray}github user: ${cDarkGray}${sBold}$user${sReset}"
+fi
+
+id=${id:=`hostname -s`-$USER-`date +'%Y-%m-%d-%Hh%M-%Ss'`}
+last=none
+if [ -f $priv/latest.id ]
+then
+  last=`cat $priv/latest.id`
+  [ -t 1 ] && echo -e "${cDarkGray}last cert gen id: ${cDarkGray}${sBold}${last}${sReset}"
+fi
+
+
+# echo utils
+
+abort() {
+   if [ -t 1 ]
+   then
+     echo -e "${sBold}${cRed}${1}${sReset}"
+     shift
+     if [ $# -gt 0 ]
+     then
+       echo "Try:"
+       while [ $# -gt 0 ]
+       do
+         echo -e "${cGreen}${1}${cNormal}"
+         shift
+       done
+     fi
+   else
+      echo "$1"
+      shift
+      if [ $# -gt 0 ]
+      then
+        echo "Try:"
+        while [ $# -gt 0 ]
+        do
+          echo "$1"
+          shift
+        done
+     fi
+   fi
+   exit 1
+}
+
+abortc() {
+   if [ -t 1 ]
+   then
+     echo -e "${sBold}${cRed}${1}${sReset}"
+     shift
+     if [ $# -gt 0 ]
+     then
+       echo -e "${sBold}${cGreen}Try:${sReset}"
+       while [ $# -gt 0 ]
+       do
+         echo -e "${cGreen}${1}${cNormal}"
+         shift
+       done
+     fi
+   else
+      echo "$1"
+      shift
+      if [ $# -gt 0 ]
+      then
+        echo "Try:"
+        while [ $# -gt 0 ]
+        do
+          echo "$1"
+          shift
+        done
+     fi
+   fi
+   exit 1
+}
+
+headline() {
+   echo
+   if [ -t 1 ]
+   then
+      echo -e "${cYellow}${*}${cNormal}"
+   else
+      echo "$*"
+   fi
+}
+
+headlinew() {
+   echo
+   if [ -t 1 ]
+   then
+      echo -e "${cRed}${sBold}${*}${cNormal}${sReset}"
+   else
+      echo "$*"
+   fi
+}
+
+warn() {
+   if [ -t 1 ]
+   then
+      echo -e "${cRed}${*}${cNormal}"
+   else
+      echo "$*"
+   fi
+}
+
+info() {
+   if [ -t 1 ]
+   then
+      echo -e "${cGreen}${*}${cNormal}"
+   else
+      echo "$*"
+   fi
+}
+
+headlineb() {
+   echo
+   if [ -t 1 ]
+   then
+      echo -e "${sBold}${cYellow}${*}${sReset}"
+   else
+      echo "$*"
+   fi
+}
+
+headlinei() {
+   echo
+   if [ -t 1 ]
+   then
+      echo -e "${sItalic}${cYellow}${*}${sReset}"
+   else
+      echo "$*"
+   fi
+}
+
+hint() {
+   echo
+   if [ -t 1 ]
+   then
+      if echo "$1" | grep -q '^[A-Z]'
+      then
+        echo -e "${cGreen}Hint: ${*}${cNormal}"
+      else
+        echo -e "${cGreen}Hint:\n${*}${cNormal}"
+      fi
+   else
+      echo "Hint: $*"
+   fi
+}
+
+carg() {
+  [ $# -ge 2 ] || abort "Invalid number of args: $# for 'carg' but expect 2 or more"
+  count=$1
+  shift
+  name=$1
+  shift
+  expected=$#
+  if [ $expected -ne $count ]
+  then
+    if [ $# -gt 0]
+    then
+      abort "Invalid $count args for '$name' but expect $# ($@)"
+    else
+      abort "Invalid $count args for '$name' but expect none"
+    fi
+  fi
+}
 
 # commands
 
